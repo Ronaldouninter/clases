26500369961f37cd7511d82b1ca4e9134ea7b97d
Minilab-ing10 <Minilab-ing10@MINILAB-ING10>
2025-08-25 08:25:05 -0600

    Agregar archivos de proyecto.

---
+++ clases/Acciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases
+{
+    internal class Acciones
+    {
+        public int sumar (int a , int b)
+        {
+            return a + b;   
+            
+        }
+        public int restar (int a , int b)
+        {
+            return a - b;
+        }
+        public int multiplicar (int a, int b)
+        {
+            return a * b;
+        }
+        public int dividir (int a, int b)
+        {
+            return a / b;
+        }
+
+        
+    }
+}
---
+++ clases/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acciones acciones = new Acciones();
+            while (true)
+            {
+                switch (menu())
+                {
+                    case 1:
+
+                        Console.WriteLine("Ingresa un numero");
+                        int a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa otro numero");
+                        int b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es " + acciones.sumar (a,b));
+                        break;
+
+                     case 2:
+                        Console.WriteLine("Dame un numero");
+                        int a = int.Parse (Console.ReadLine());
+                        Console.WriteLine("Dame otro numero");
+                        int b = int.Parse((string) Console.ReadLine());
+                        Console.WriteLine("La resta es " + acciones.restar(a,b));
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+        static int menu()
+        {
+            int opc;
+            Console.WriteLine("1) SUMAR");
+            Console.WriteLine("2) RESTAR");
+            Console.WriteLine("3) MULTILPLICAR");
+            Console.WriteLine("4) DIVIDIR");
+            Console.WriteLine("5) SALIR");
+           opc = int.Parse(Console.ReadLine());
+            return opc;
+        }
+    }
+}
